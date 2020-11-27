699a7af9c6917c3a0253641b1d9408abdb97fdff
Vanessa Ho <vanessa.ho@externals.adidas.com>
2020-11-27 11:48:05 +0100

    Correctly send params to sellbyId and add new route to GET products by id

--- api/server.js
+++ api/server.js
@@ -2,27 +2,38 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const products = require('./controllers/products')
+const getProduct = require('./controllers/getProduct')
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
 // Get all the products with stock
-app.get('/products', (req, res) => { 
-  products.getList(productList => 
-    res.send(JSON.stringify(productList))
+app.get('/products', (req, res) => {
+  products.getList(productList =>
+    res.json(productList)
+  )
+})
+
+// Get all the products with stock
+app.get('/products/:id', (req, res) => {
+  products.getProduct(
+    req.params.id,
+    (product) => {
+      res.json(product)
+    }
   )
 })
 
 // Sell a product by id
 app.put('/products/:id/sell', (req, res) => {
   products.sellById(
-    req.params['id'], 
-    (ok) => {
-      if (ok) {
-        res.status(200).send(`{ "response": "inventory updated" }`)
+    req.params.id,
+    (prodAvailable) => {
+      if (prodAvailable) {
+        res.status(200).json({ response: 'inventory updated' })
       } else {
-        res.status(200).send(`{ "response": "product stock not available" }`)
+        res.status(200).json({ response: 'product stock not available' })
       }
     }
   )
