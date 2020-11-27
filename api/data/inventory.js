c828708a9118c13e2e8587b11253fc1a5058f44c
Vanessa Ho <vanessa.ho@externals.adidas.com>
2020-11-27 11:43:20 +0100

    Correctly retrieve articles and update inventory

--- api/data/inventory.js
+++ api/data/inventory.js
@@ -3,21 +3,23 @@ const path = require('path')
 
 const INVENTORY_FILE_PATH = '../../files/inventory.json'
 
-const getByIds = (params, callback) => {
-  const articlesIds = params['ids']
-  
+const getByIds = (params, onDone) => {
+  const articlesIds = params.ids
+
   fs.readFile(path.join(__dirname, INVENTORY_FILE_PATH), (err, data) => {
+    if (err) throw new Error('error reading file')
+
     const inventoryList = JSON.parse(data)
     const inventory = inventoryList.inventory
-    
+
     // if the article is not in the requested list, filter it
     const articles = inventory.filter(article => !articlesIds.includes(article.id))
 
-    callback(articles)
+    onDone(articles)
   })
 }
 
-const updateAmounts = (articlesPurchased, callback) => {
+const updateAmounts = (articlesPurchased, onDone) => {
   fs.readFile(path.join(__dirname, INVENTORY_FILE_PATH), (err, data) => {
     if (err) throw err
     const inventoryList = JSON.parse(data)
@@ -26,28 +28,32 @@ const updateAmounts = (articlesPurchased, callback) => {
 
     const newInventoryStock = inventory.reduce(
       (currentInventory, article) => {
-        const articleRequired = articlesPurchased.find(article => article.art_id === article.id)
+        const articleRequired = articlesPurchased.find(
+          purchasedArticle => purchasedArticle.art_id === article.art_id
+        )
+
         if (articleRequired) {
-          const amountRequired = articleRequired.amount_of
-          const articleStock = article.stock
-          
+          const amountRequired = parseInt(articleRequired.amount_of, 10)
+          const articleStock = parseInt(article.stock, 10)
+
           // if this article has less quantity it will determine how many products I can sell so it should be used as maximum stock
-          return currentInventory.push({
+          currentInventory.push({
             ...article,
-            stock: articleStock - amountRequired
+            stock: `${articleStock - amountRequired}`
           })
         } else {
-          return currentInventory.push(article)
+          currentInventory.push(article)
         }
+        return currentInventory
       },
       []
     )
 
     // write the file with the new stock
     fs.writeFile(
-      path.join(__dirname, INVENTORY_FILE_PATH), 
+      path.join(__dirname, INVENTORY_FILE_PATH),
       JSON.stringify({ inventory: newInventoryStock }, null, 4),
-      callback
+      onDone
     )
   })
 }
